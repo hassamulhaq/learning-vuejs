28dbe06232569b6220579705d4c850bb86485071
hassamulhaq <developer.hassam@gmail.com>
2022-10-31 18:22:55 +0500

    Vue router configured

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 //import HelloWorld from './components/HelloWorld.vue'
-import Product from "./components/Product.vue";
+//import Product from "./components/Product.vue";
 
 
 </script>
 
 <template>
-  <Product title="Single Product" />
+  <router-link to="/">Home</router-link>
+  <router-link to="/about-us">About Us</router-link>
+  <router-view></router-view>
 </template>
 
 <style scoped>
